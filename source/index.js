2f705a2cab61ff1d57ee2f6a934ea5c5b2b48632
Simon Lu <lusimon95@gmail.com>
2017-09-11 21:40:31 -0700

    updates for mp1 release

--- source/index.js
+++ source/index.js
@@ -1,9 +1,7 @@
 /*
  * This is the main entry point for Webpack, the compiler & dependency loader.
  * All files that are necessary for your web page and need to be 'watched' for changes should be included here!
- *
- * For this MP, all of the files you need to modify have already been created and included here.
- * Note that you do not need to change any Javascript files for this MP unless you are attempting the optional learning. */
+ */
 
 // HTML Files
 require('./html/index.html');
